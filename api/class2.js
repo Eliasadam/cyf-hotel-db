52ba90f5eef2ca469f6a8601be915719d4850d16
eliasadam <elias-adam@hotmail.co.uk>
2018-08-14 20:32:18 +0100

    completed homework 2,3,4 but having trouble with 4 and 5. please review my homework. thank you for your time in advance

--- api/class2.js
+++ api/class2.js
@@ -6,28 +6,88 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(filename);
 // db.run("PRAGMA foreign_keys = ON");
 
-router.get('/customers', function(req, res) {
+router.get('/customers', function (req, res) {
   var sql = 'select * from customers';
 
-  db.all(sql, [], (err, rows ) => {
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
     res.status(200).json({
+
       customers: rows
     });
   });
 });
 
 
-router.get('/customers/:id', function(req, res) {
+
+router.get('/customers/:id', function (req, res) {
+  var customer_id = req.param("id");
+  var sql = `select * from customers where id= ${customer_id}`;
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      customers: rows
+    });
+  });
+  // res.send()
   // TODO: add code here
 });
 
 
-router.get('/customers/:surname', function(req, res) {
+router.get('/customers/search/:surname', function (req, res) {
+  var customers_surname = req.param("surname");
+
+  var sql = `select * from customers where surname like '%${customers_surname}%';`
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      customers: rows
+    });
+  });
   // TODO: add code here
 });
 
 
-router.post('/customers/', function(req, res) {
+router.post('/customers/', function (req, res) {
+
+  var sql = `INSERT INTO customers (title, first_name, surname, email) VALUES ('${req.body.title}', '${req.body.first_name}','${req.body.surname}','${req.body.email}');`
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      customers: req.body
+    });
+  });
+
   // EXPECTED JSON Object:
   // {
   //   title: 'Mr',
@@ -40,7 +100,22 @@ router.post('/customers/', function(req, res) {
 });
 
 
-router.put('/customers/:id', function(req, res) {
+router.put('/customers/:id', function (req, res) {
+
+  var sql = `update customers set first_name = '${req.body.first_name}',
+   surname = '${req.body.surname}', email = '${req.body.email}'  where id= '${req.params.id}';`
+  db.run(sql, [], (err) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    res.status(200).json({
+
+      customers: req.body
+    });
+  });
   // EXPECTED JSON Object:
   // {
   //   title: 'Mr',
@@ -52,12 +127,84 @@ router.put('/customers/:id', function(req, res) {
   // TODO: add code here
 });
 
+router.delete('/customers/delete/:id', function (req, res) {
 
+  var sql = `delete from customers where id =?;`
+  db.run(sql, [req.body.id], (err) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    res.status(200).json({
+
+      customers: req.body
+    });
+  });
+});
+
+
+
+router.delete('/reservations/delete/:id', function (req, res) {
+
+  var sql = `delete from reservations where id =?;`
+  db.run(sql, [req.body.id], (err) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    res.status(200).json({
+
+      reservations: req.body
+    });
+  });
+});
+
+
+router.get('/reservations', function (req, res) {
+  var sql = 'select * from reservations';
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      reservations: rows
+    });
+  });
+});
 // get '/reservations'
 // TODO: add code here
 
+router.put('/reservations/:id', function (req, res) {
 
-// get '/reservations/:id'
+  var sql = `select * from reservations where id= ${req.param("id")}`;
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      reservations: rows
+    });
+  });
+  // res.send()
+  // TODO: add code here
+});
+
 // TODO: add code here
 
 
@@ -65,13 +212,42 @@ router.put('/customers/:id', function(req, res) {
 // TODO: add code here
 
 
-// get '/reservations/starting-on/:startDate'
+// 
 // TODO: add code here
 
 
-// get '/reservations/active-on/:date'
-// TODO: add code here
 
+
+
+router.post('/reservations/', function (req, res) {
+
+  var sql = `INSERT INTO reservations (customer_id, room_id, check_in_date, room_price)
+   VALUES ('${req.body.customers_id}', '${req.body.room_id}','${req.body.check_in_date}','${req.body.room_price}');`
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      reservations: req.body
+    });
+  });
+
+  // EXPECTED JSON Object:
+  // {
+  //   title: 'Mr',
+  //   firstname: 'Laurie',
+  //   surname: 'Ainley',
+  //   email: 'laurie@ainley.com'
+  // }
+
+  // TODO: add code here
+});
 
 // post '/reservations'
 // EXPECTED JSON Object:
@@ -83,13 +259,51 @@ router.put('/customers/:id', function(req, res) {
 //   room_price: 129.90
 // }
 // TODO: add code here
+// get '/reservations/active-on/:date'
+// TODO: add code here
+router.get('/reservations/check_in_date/:check_out_date', function (req, res) {
 
+  var sql = `select * from reservations where check_out_date=?`;
+
+  db.all(sql, [req.params.check_out_date], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      reservations: rows
+    });
+  });
+
+});
 
 // get `/detailed-invoices'
 // TODO: add code here
 
 
-// get `/reservations/details-between/:from_day/:to_day`
+router.get(`/reservations/active-on/:chech_in_date`, function (req, res) {
+
+  var sql = `select * from reservations where check_in_date = ${req.params.check_in_date}`;
+
+  db.run(sql, [], (err, rows) => {
+
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+
+      reservations: rows
+    });
+  });
+
+});
 // TODO: add code here
 
 module.exports = router;
